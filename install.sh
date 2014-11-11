8f001bb86c6b96e7e0a1d2592820880899b6ac08
Ian Marcinkowski <ian@desrt.ca>
2014-11-11 13:43:13 -0500

    add .run to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# Directories
+mkdir -p ~/.local/bin
+mkdir ~/.run
+mkdir ~/.vim
+mkdir ~/.zsh
+
+
 cp zshrc ~/.zshrc
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp oh-my-zsh/ianmarcinkowski.zsh-theme ~/.oh-my-zsh/themes/
-mkdir -p ~/.local/bin
 cp local-bin/* ~/.local/bin/
-cp -r vim ~/.vim
-cp -r zsh ~/.zsh
+cp -r vim/* ~/.vim/
+cp -r zsh/* ~/.zsh/
 cp git/gitignore ~/.gitignore
 cp hgrc ~/.hgrc
